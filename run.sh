5f97dd034da03ead5e2b046476402f457c6ef119
MengjiaoH <u1078078@utah.edu>
2023-01-07 10:36:37 -0700

    add run.sh

---
+++ run.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+./DepthByVoxel /home/mengjiao/Desktop/SurfaceBoxplot_CXX/datasets/wind_pressure_200/data_raw/Lead_00/ 15 240 121 2 /home/mengjiao/Desktop/SurfaceBoxplot_CXX/datasets/wind_pressure_200/Lead_00.txt 
